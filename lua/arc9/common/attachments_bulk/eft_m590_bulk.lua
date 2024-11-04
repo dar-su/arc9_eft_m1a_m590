e1e954a92781a1838ee89677860fcf554c1e8e70
Darsu <95997959+dar-su@users.noreply.github.com>
2024-11-05 03:48:57 +0700

    Update eft_m590_bulk.lua

--- lua/arc9/common/attachments_bulk/eft_m590_bulk.lua
+++ lua/arc9/common/attachments_bulk/eft_m590_bulk.lua
@@ -171,7 +171,8 @@ ATT.Attachments = {
         Category = {"eft_optic_large", "eft_optic_medium", "eft_optic_small"},
         Pos = Vector(-2.5, 0, -0.37),
         Ang = Angle(0, 0, 0),
-        ExtraSightDistance = 3
+        ExtraSightDistance = 3,
+        ExcludeElements = {"IronsBlockingSight"},
     },
     {
         PrintName = "Backup",
